4bd79444db0eedf7dffe417fd77f4a2fd229cf1a
xuweisong <xuweisong37@126.com>
2020-11-23 15:36:12 +0800

    定义采集

--- app/Console/Commands/Collects.php
+++ app/Console/Commands/Collects.php
@@ -38,7 +38,7 @@ class Collects extends Command
     public function handle()
     {
         $url = 'https://apps.game.qq.com/cmc/zmMcnContentInfo?r0=jsonp&source=web_pc&type=0&docid=14961237808844876072&r1=jQuery19104658286916897647_1606099368113&_=1606100893850';
-// 定义采集规则
+        // 定义采集规则
         $detailData=curl_get1($url);
 
         dd($detailData);
