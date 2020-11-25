c58948e014d08ec3b313e9787606bf4c69865c8f
xuweisong <xuweisong37@126.com>
2020-11-25 16:20:09 +0800

    百度百科采集文件修改

--- app/Collect/team/kpl/baidu_baike.php
+++ app/Collect/team/kpl/baidu_baike.php
@@ -5,17 +5,18 @@ namespace App\Collect\team\kpl;
 use App\Models\CollectResultModel;
 use App\Models\MissionModel;
 use App\Services\CollectResultService;
+use QL\QueryList;
 
 
 class baidu_baike
 {
     public function collect($arr)
     {
-        $model = new CollectResultService();
+        $resultService = new CollectResultService();
 
         $id = $arr['detail']['id'] ?? '';
         $url = $arr['detail']['url'] ?? '';
-        $res = $model->getCollectData($url);
+        $res = $this->getCollectData($url);
         $cdata = [];
         if (!empty($res)) {
             $cdata = [
@@ -28,11 +29,50 @@ class baidu_baike
 
             ];
             //处理战队采集数据
-            $rt=$model->doCollect($arr['mission_id'],$id,$cdata);
+            $rt=$resultService->doCollect($arr['mission_id'],$id,$cdata);
             if ($rt) {
                 return true;
             }
         }
 
+    }
+
+    /**
+     * @param string $url
+     * @return array $res
+     */
+    public function getCollectData($url='')
+    {
+        $res=[];
+        if ($url && strlen($url)>=6) {
+            $ql = QueryList::get($url);
+            $res['describe'] = $ql->find('.main-content  .lemma-summary')->text();//百度百科抓取 战队简介
+            $res['logo'] = $ql->find('.side-content img')->src;//战队logo
+            $baseInfoNames = $ql->find('.basic-info .name')->texts();//基础信息名称
+            $baseInfoValues = $ql->find('.basic-info .value')->texts();//名称对应值
+            $baseInfos = [];
+            $tmp_arr = array();
+            if ($baseInfoValues) {
+                foreach ($baseInfoValues as $key => $val) {
+                    $name = preg_replace("/(\s|\&nbsp\;|　|\xc2\xa0)/", " ", strip_tags($baseInfoNames[$key]));
+                    $name = preg_replace('# #', '', $name);
+                    if (!in_array($name, $tmp_arr)) {
+                        array_push($tmp_arr, $name);
+                        if (strpos($val, '主要荣誉') !== false) {
+                            $arrtemp = explode('主要荣誉', $val);
+                            $val = $arrtemp[1] ?? '';
+                            $val = trim(trim($val, '收起'));
+                        }
+                        $baseInfos[$key] = [
+                            'name' => $name,
+                            'value' => $val
+                        ];
+                    }
+                }
+            }
+
+            $res['base_info'] = $baseInfos;
+        }
+        return $res;
     }
 }
--- app/Services/CollectResultService.php
+++ app/Services/CollectResultService.php
@@ -4,48 +4,10 @@ namespace App\Services;
 
 use App\Models\CollectResultModel;
 use App\Models\MissionModel;
-use QL\QueryList;
 
 class CollectResultService
 {
-    /**
-     * @param string $url
-     * @return array $res
-     */
-    public function getCollectData($url='')
-    {
-        //
-        if ($url && strlen($url)>=6) {
-            $ql = QueryList::get($url);
-            $res['describe'] = $ql->find('.main-content  .lemma-summary')->text();//百度百科抓取 战队简介
-            $res['logo'] = $ql->find('.side-content img')->src;//战队logo
-            $baseInfoNames = $ql->find('.basic-info .name')->texts();//基础信息名称
-            $baseInfoValues = $ql->find('.basic-info .value')->texts();//名称对应值
-            $baseInfos = [];
-            $tmp_arr = array();
-            if ($baseInfoValues) {
-                foreach ($baseInfoValues as $key => $val) {
-                    $name = preg_replace("/(\s|\&nbsp\;|　|\xc2\xa0)/", " ", strip_tags($baseInfoNames[$key]));
-                    $name = preg_replace('# #', '', $name);
-                    if (!in_array($name, $tmp_arr)) {
-                        array_push($tmp_arr, $name);
-                        if (strpos($val, '主要荣誉') !== false) {
-                            $arrtemp = explode('主要荣誉', $val);
-                            $val = $arrtemp[1] ?? '';
-                            $val = trim(trim($val, '收起'));
-                        }
-                        $baseInfos[$key] = [
-                            'name' => $name,
-                            'value' => $val
-                        ];
-                    }
-                }
-            }
 
-            $res['base_info'] = $baseInfos;
-        }
-        return $res;
-    }
     //处理采集数据并且同步到数据库
     public function doCollect($mission_id,$id,$data){
         $collectModel = new CollectResultModel();
