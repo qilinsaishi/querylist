0ad6a668c065240acc02c6c3fe935fbd85df220d
xuweisong <xuweisong37@126.com>
2021-01-05 13:50:20 +0800

    调试玩家队员

--- app/Services/MissionService.php
+++ app/Services/MissionService.php
@@ -15,7 +15,7 @@ class MissionService
     public function collect($game = "", $source = "", $mission_type = '')
     {
         //获取爬取任务列表
-        $mission_list = $this->getMission($game, $source, $mission_type, 200);
+        $mission_list = $this->getMission($game, $source, $mission_type, 400);
         $collectModel = new CollectModel();
         $missionModel = new MissionModel();
         //初始化空的类库列表
