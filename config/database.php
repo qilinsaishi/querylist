cf2458cc2ec1914456601dea27721c0825ba6f8e
xiaodong chen <cxd032404@hotmail.com>
2021-01-11 18:31:37 +0800

    merge

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
 			'driver' => 'mysql',
 			'host' => env('DB_HOST', '127.0.0.1'),
 			'port' => env('DB_PORT', '3306'),
-			'database' => 'DB_DATABASE_QUERYADMIN',
+			'database' => env('DB_DATABASE_QUERYADMIN', 'forge'),
 			'username' => env('DB_USERNAME_QUERYADMIN', 'forge'),
 			'password' => env('DB_PASSWORD', 'forge'),
 			'unix_socket' => env('DB_SOCKET', ''),
