7155ef4d41380055820fc8af1124d584d5d6f6fa
xiaodong chen <cxd032404@hotmail.com>
2021-02-09 16:23:51 +0800

    merge

--- app/Http/Controllers/lolIndexController.php
+++ app/Http/Controllers/lolIndexController.php
@@ -6,6 +6,7 @@ use App\Libs\AjaxRequest;
 use App\Libs\ClientServices;
 use App\Models\CollectResultModel;
 use App\Models\Admin\DefaultConfig;
+use App\Services\Data\DataService;
 use App\Services\Data\ExtraProcessService;
 use Illuminate\Http\Request;
 
@@ -13,8 +14,6 @@ use Illuminate\Http\Request;
 use QL\QueryList;
 use GuzzleHttp\Client;
 
-use App\Services\Data\PrivilegeService;
-use App\Services\Data\RedisService;
 
 class lolIndexController extends Controller
 {
@@ -26,65 +25,11 @@ class lolIndexController extends Controller
 
     public function get()
     {
-        $redisService = new RedisService();
-        $privilegeService = new PrivilegeService();
         $data=$this->payload;
-        $return = [];
-        $functionList = $privilegeService->getFunction($data);
-        foreach($data as $name => $params)
-        {
-            $dataType = $params['dataType']??$name;
-            if(isset($functionList[$dataType]))
-            {
-                $toSave = 1;
-                $dataArr = $redisService->processCache($dataType,$data[($params['cacheWith']??"")]??$params);
-                if(is_array($dataArr))
-                {
-
-                    //$return[$dataType] = $cache;
-                    $toSave = 0;
-                }
-                else
-                {
-                    $functionInfo = $functionList[$dataType];
-                    $class = $functionInfo['class'];
-                    $function = $functionInfo['function'];
-                    //$params = $data[$dataType];
-                    $d = $class->$function($params);
-                    $functionCount = $functionInfo['functionCount'];
-                    $functionProcess = $functionInfo['functionProcess']??"";
-                    $functionProcessCount = $functionInfo['functionProcessCount']??"";
-                    if(!$functionCount || $functionCount=="")
-                    {
-                        $count = 0;
-                    }
-                    else
-                    {
-                        $count = $class->$functionCount($params);
-                    }
-                    if($functionProcessCount!="")
-                    {
-                        $count = $privilegeService->$functionProcessCount($d,$functionList,$params);
-                    }
-                    if($functionProcess!="")
-                    {
-                        $d = $privilegeService->$functionProcess($d,$functionList,$params);
-                    }
-                    $dataArr = ['data'=>$d,'count'=>$count];
-                }
-                if($toSave==1)
-                {
-                    $redisService->saveCache($dataType,$data[($params['cacheWith']??"")]??$params,$dataArr);
-                }
-                if(isset($dataType) && $dataType='informationList') {
-                    $dataArr["data"] = (new ExtraProcessService())->process($dataType,$dataArr["data"]);
-                }
-
-                $return[$name] = $dataArr;
-            }
-        }
+        $return = (new DataService())->getData($data);
         return $return;
     }
+
     public function refresh()
     {
         $redisService = new RedisService();
