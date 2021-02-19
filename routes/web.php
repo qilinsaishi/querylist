6b437766adfb95aa7157b2ac7206d72b65f96a32
xiaodong chen <cxd032404@hotmail.com>
2021-02-19 14:16:01 +0800

    merged

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('home','HomeController@index');
 Route::post('home/list','HomeController@lists');
 Route::get('home/test','HomeController@test');
 Route::post('get','IndexController@get');
-Route::post('sitemap','indexController@sitemap');
+Route::post('sitemap','IndexController@sitemap');
 Route::any('lol/team_info','HomeController@teamInfo');
 Route::any('lol/refresh','lolIndexController@refresh');
 Route::any('refresh','IndexController@refresh');
