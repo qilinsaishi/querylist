7f7ecd57e000815d3453d15eb6bfbff28fa29fe2
xuweisong <xuweisong37@126.com>
2021-03-19 13:51:53 +0800

    修改报错

--- app/Libs/AjaxRequest.php
+++ app/Libs/AjaxRequest.php
@@ -222,10 +222,10 @@ class AjaxRequest
     {
         $res=[];
         //判断url是否有效
-        /*$headers=get_headers($url,1);
+        $headers=get_headers($url,1);
         if(!preg_match('/200/',$headers[0])){
             return  [];
-        }*/
+        }
         if ($url && strlen($url)>=6) {
             $ql = QueryList::get($url);
             $res['logo'] = $ql->find('#sharePic')->src;//战队logo
