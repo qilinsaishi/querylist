837b4d2c0f0d94a4c70956337a3ea1caa781ba85
xiaodong chen <cxd032404@hotmail.com>
2021-03-29 17:24:40 +0800

    merge

---
+++ app/Services/Data/IntergrationService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Services\Data;
+use App\Services\Data\RedisService;
+use App\Models\TeamModel;
+use App\Models\Team\TotalTeamModel as TotalTeamModel;
+use App\Models\Team\TeamMapModel as TeamMapModel;
+use App\Models\Team\TeamNameMapModel as TeamNameMapModel;
+
+use App\Models\PlayerModel;
+use App\Models\Player\TotalPlayerModel as TotalPlayerModel;
+use App\Models\Player\PlayerMapModel as PlayerMapModel;
+use App\Models\Player\PlayerNameMapModel as PlayerNameMapModel;
+
+class IntergrationService
+{
+    //team_id:team_info表的主键
+    //force:强制重新获取 1是0否
+    public function getTeamInfo($team_id,$force = 1)
+    {
+        if($force==1)
+        {
+            $oTeam = new TeamModel();
+            $oTeamMap = new TeamMapModel();
+            //找到单条详情
+            $singleTeamInfo = $oTeam->getTeamById($team_id);
+            //找到
+            if(isset($singleTeamInfo['team_id']))
+            {
+                //获取当前映射
+                $singleMap = $oTeamMap->getTeamByTeamId($singleTeamInfo['team_id']);
+                //找到映射
+                if(isset($singleMap['tid']))
+                {
+                    $teamMapList = $oTeamMap->getTeamByTid($singleMap['tid']);
+                    print_R($teamMapList);
+                    die();
+                }
+                else//没找到
+                {
+                    //创建映射
+                }
+            }
+            else//没找到
+            {
+                $return = [];
+            }
+        }
+    }
+
+}
