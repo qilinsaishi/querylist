06336daed83782319c125d1829166fba9e6f41f0
justin.chen <cxd032404@hotmail.com>
2021-06-16 10:06:27 +0800

    merge

--- app/Models/Match/scoregg/matchListModel.php
+++ app/Models/Match/scoregg/matchListModel.php
@@ -111,10 +111,11 @@ class matchListModel extends Model
         //比赛开始时间start=1表示启动开始时间条件
         if (isset($params['recent']) && $params['recent'] > 0) {
             $currentTime = time();
-            $start_time = date("Y-m-d", $currentTime-3*86400);
+            $start_time = date("Y-m-d", $currentTime);
             $end_time = date("Y-m-d", $currentTime+2*86400);
             $match_list = $match_list->where("start_time", '>=', $start_time);
             $match_list = $match_list->where("start_time", '<=', $end_time);
+            $orderByList = [["start_time","asc"]];
         }
         //比赛开始时间start=1表示启动开始时间条件
         if (isset($params['next_try']) && $params['next_try'] > 0) {
